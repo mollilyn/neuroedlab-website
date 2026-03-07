46148a08e02a2c11328648db096c1ef9c0421d5a
Jack Rowson <jack.rowson@outlook.com>
2026-03-07 08:03:08 +0000

    Test CI/CD 2

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <main className="flex flex-col items-center gap-2 text-center">
-        <h1 className="text-2xl font-semibold">NeuroEd Lab Website</h1>
+        <h1 className="text-2xl font-semibold">NeuroEd Lab Website - Test Edit</h1>
         <p className="text-base">Development Build</p>
       </main>
     </div>
